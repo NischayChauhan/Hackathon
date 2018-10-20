71aadeac0dd7e805a65f0f7a85341776696e39d7
knack <knack@me.com>
2018-10-20 15:30:59 +0530

    Started PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dr. Sanguetio</title>
+</head>
+<body>
+	<div>
+		<a href="php/signUp.php">SignUp</a>
+		<a href="php/logIn.php">LogIn</a>
+	</div>
+	<div>
+		<a href="php/contri.php">Contribute</a>
+		<a href="php/search.php">Search For Blood</a>
+		<a href="php/doctor.php">Doctors Appointment</a>		
+	</div>	
+
+</body>
+</html>
